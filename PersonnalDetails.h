1e32dbb7123101a60571170f3aff30a521248723
Sajeewa <gitconflict@gmail.com>
2015-06-13 19:16:22 +0530

    remove several field

--- PersonnalDetails.h
+++ PersonnalDetails.h
@@ -7,7 +7,6 @@ struct PersonnalDetails
         char firstName[4];
         char middleName[4];
         char lastName[4];
-        char address[4];
-        char telephoneNumber[4];
+      
     
 };
