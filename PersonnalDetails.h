62fb969603369ae008379aaa5f305da4bacde228
Sajeewa <gitconflict@gmail.com>
2015-06-13 18:14:00 +0530

    remove fields from struct

--- PersonnalDetails.h
+++ PersonnalDetails.h
@@ -9,11 +9,5 @@ struct PersonnalDetails
         char lastName[4];
         char address[4];
         char telephoneNumber[4];
-        char emailAddress[13];
-        char NICnumber[11];
-        char primarySchool[4];
-        char secondarySchool[4];
-        char teritarySchool[4];
-        char workPlace[4];
-		char dateOfBirth[11];
+    
 };
